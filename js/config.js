67974dc308c1978539a39f6d77044b9485b1a980
aloha1003 <knives1003@gmail.com>
2013-10-02 09:49:54 +0800

    third commit

--- js/config.js
+++ js/config.js
@@ -3,9 +3,7 @@ define([], function () {
   return  {
   		server :  "server/phpserver/index.php/",
   		file_upload :  "server/phpserver/file.php",
-  		facebookappid: '231524443667760'
-  		//server :  "http://aloha1003.ap01.aws.af.cm/server/phpserver/index.php/",
-  		//file_upload :  "http://aloha1003.ap01.aws.af.cm/server/phpserver/file.php"
+  		facebookappid: 'YourfacebookAPPID'
   	};
 });
 
